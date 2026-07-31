5f6f1b2d5d222e1619945b660cce523e0e3e05af
Ahmad Khrayzat <162881542+kernel10452@users.noreply.github.com>
2026-07-31 04:19:45 +0300

    Update project name and clean up settings
    Removed unused plugin configurations and updated project name.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,6 @@ pluginManagement {
         mavenCentral()
     }
 }
-plugins {
-    id("com.highcapable.sweetdependency") version "__SWEET_DEPENDENCY_VERSION__"
-    id("com.highcapable.sweetproperty") version "__SWEET_PROPERTY_VERSION__"
-}
-sweetProperty {
-    rootProject { all { isEnable = false } }
-}
-rootProject.name = "__PROJECT_NAME__"
+
+rootProject.name = "MyServerApp"
 include(":app")
